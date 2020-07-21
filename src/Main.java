506a1c12d51fc89b59125824d4b3b7bc6860a684
Nikolay <nikolai-vyalkov@yandex.ru>
2020-07-21 23:18:03 +0500

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
      public static void main(String[] args) {
         int originbalance = 100;
-        int incomingbalance = 1299;
+        int incomingbalance = 1100;
         double bonus = 0;
-        if (incomingbalance >= 1100) {
+        if (incomingbalance > 1000) {
             bonus = Math.floor(incomingbalance / 100);
         }
 
